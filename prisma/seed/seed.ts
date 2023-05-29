86c4f78537ac9ee8f28828be8e5d4522fd13ea7e
Oleksandr Sereda <sereda.san@gmail.com>
2023-05-29 21:26:46 +0300

    Add prisma schema, seed database

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+
+import celentano from "./celentano.json";
+import kfc from "./kfc.json";
+import lvivcroissants from "./lvivcroissants.json";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.dish.deleteMany();
+  await prisma.restaurant.deleteMany();
+
+  await Promise.all(
+    [celentano, kfc, lvivcroissants].map(async ({ restaurant, dishes }) => {
+      const newRest = await prisma.restaurant.create({
+        data: { ...restaurant, dishes: { create: dishes } },
+      });
+      console.log("🚧", newRest);
+    })
+  );
+  console.log("🚧", "DONE");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
